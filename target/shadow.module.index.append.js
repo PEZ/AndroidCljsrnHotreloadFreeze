5ce57966e1ed11e62c6627e2b248cf84d2f9087c
Peter Strömberg <pez@pezius.com>
2021-07-06 19:56:23 +0200

    Gitignore target

--- target/shadow.module.index.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('index');
-
-try { awesome_project.core.init(); } catch (e) { console.error("An error occurred when calling (awesome-project.core/init)"); throw(e); }
